2b1df5a71f2b0b1cf748d211434f74f841f612d8
nathanestill <nathan.estill@students.olin.edu>
2019-04-09 20:43:09 -0400

    adding somewhat of a skeleton

---
+++ BikeLight/BikeLight.ino
@@ -0,0 +1,25 @@
+#include "pt.h"
+static int protothread1_flag, protothread2_flag;
+
+static int inputs(struct pt *pt){
+
+}
+static int outputs(struct pt *pt){
+
+}
+static struct pt pt1, pt2;
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  PT_INIT(&pt1);
+  PT_INIT(&pt2);
+  /*
+   * Then we schedule the two protothreads by repeatedly calling their
+   * protothread functions and passing a pointer to the protothread
+   * state variables as arguments.
+   */
+  while(1) {
+    inputs(&pt1);
+    outputs(&pt2);
+  }
+}

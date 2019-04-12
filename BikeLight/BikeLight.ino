98a1fbaa678bae23eab7380f37bcbf1f1a66edd0
Abby Fry <abbymfry@gmail.com>
2019-04-12 13:20:41 -0400

    wokring on output thread

--- BikeLight/BikeLight.ino
+++ BikeLight/BikeLight.ino
@@ -1,16 +1,24 @@
 #include "pt.h"
 static int protothread1_flag, protothread2_flag;
 
+static int ButtonState;
+
 static int inputs(struct pt *pt){
 
 }
 static int outputs(struct pt *pt){
 
+//based off of changes in button high, move to next bike light mode
+
+//Config 1
+
+//Config 2
+
 }
 static struct pt pt1, pt2;
 void loop() {
   // put your main code here, to run repeatedly:
-  
+
   PT_INIT(&pt1);
   PT_INIT(&pt2);
   /*

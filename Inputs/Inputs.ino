2d984236602de2c6715d7bc6412b1c4a29440904
nathanestill <nathan.estill@students.olin.edu>
2019-04-14 13:09:36 -0400

    making first pass

--- Inputs/Inputs.ino
+++ Inputs/Inputs.ino
@@ -2,9 +2,48 @@
 static int protothread1_flag, protothread2_flag;
 
 static int ButtonState;
+static int analogReader;
+int buttonPin = 7;
+int analogPin = A0;
 
 static int inputs(struct pt *pt){
+/* A protothread function must begin with PT_BEGIN() which takes a pointer to a struct pt. */
+  PT_BEGIN(pt);
 
+  /* We loop forever here. */
+  int buttonTemp = 0;
+  while(1) {
+    /* Wait until the other protothread has set its flag. */
+    PT_WAIT_UNTIL(pt, protothread2_flag != 0);
+    Serial.println("Protothread 1 running\n");
+    if(digitalRead(buttonPin)){
+    	if(!buttonTemp){
+    		ButtonState++;
+    		buttonTemp = 1;
+    		delay(20);
+    	}
+    } else {
+    	if(buttonTemp){
+    		buttonTemp = 0;
+    		delay(20);
+    	}
+    }
+
+    analogReader = analogRead(analogPin);
+
+    /* We then reset the other protothread's flag, and set our own flag so that the other protothread can run. */
+    protothread2_flag = 0;
+    protothread1_flag = 1;
+
+    /* And we loop. */
+  }
+
+  /* All protothread functions must end with PT_END() which takes a pointer to a struct pt. */
+  PT_END(pt);
+}
+
+static int outputs(struct pt *pt){
+	
 }
 
 static struct pt pt1, pt2;

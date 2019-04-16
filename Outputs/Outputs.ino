80e60c3828cd0a2dd567f1f8f06be7de9a4d6cf3
Abby Fry <abbymfry@gmail.com>
2019-04-15 22:59:54 -0400

    filling in skeleton

--- Outputs/Outputs.ino
+++ Outputs/Outputs.ino
@@ -1,18 +1,62 @@
 #include "pt.h"
 static int protothread1_flag, protothread2_flag;
 
-static int ButtonState;
+int ButtonState;
+int LastButtonState;
+
+const int LED1 = 9; //
+const int LED2 = 10; //
+const int LED3 = 11;
+
 
 static int outputs(struct pt *pt){
 
-//based off of changes in button high, move to next bike light mode
+  PT_BEGIN(pt);
+  int LED_index //index for the LED configuration the button is on
 
-//Config 1
+  while(1){
+    PT_WAIT_UNTIL(pt,protothread1_flag !=0)
+    Serial.println("Protothread 2 is running\n")
+    if (ButtonState!=LastButtonState){
+      //check for change in ButtonState
+      if (ButtonState == HIGH){
+        LED_index ++; //check if button is pressed and change index acordingly
+      }
+    }
 
-//Config 2
+    LastButtonState = ButtonState;
 
+    if(LED_index % 3 == 1){  //as loop run, index doesn't have to be set back to 0
+      digitalWrite(LED1,HIGH);
+      digitalWrite(LED2,LOW);
+      digitalWrite(LED3,LOW);
+    }
+
+    if(LED_index % 3 == 2){
+      digitalWrite(LED1,LOW);
+      digitalWrite(LED2,HIGH);
+      digitalWrite(LED3,LOW);
+    }
+
+    if(LED_index % 3 == 3){
+      digitalWrite(LED1,LOW);
+      digitalWrite(LED2,LOW);
+      digitalWrite(LED3,HIGH);
+    }
+
+    protothread1_flag = 0;
+    protothread2_flag = 1;
+    }
+  PT_END(pt);
 }
 static struct pt pt1, pt2;
+
+void setup(){
+  pinMode(9,OUTPUT);
+  pinMode(10,OUTPUT);
+  pinMode(11,OUTPUT);
+}
+
 void loop() {
   // put your main code here, to run repeatedly:
 
